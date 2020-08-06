1d8a79805aa6b93e03a77faf59e56f702b670426
Reo Kuchida <>
2020-08-06 10:13:59 +0900

    Add mercari search

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
 
+import requests
+
 app = Flask(__name__)
 
 LINE_CHANNEL_ACCESS_TOKEN = 'I1HVvaO4TBkowJFcYhdwARPGL3xhMogYT8tOSQ5dUQriMzfITnbKMrenHQo/+mXhtxxDhgDevovtIpN6JUL7ARZCBqImBe7Voy+kv2TTKPXl9fOA/pcZGE09o/GxxDxRl8FCswD6Ff5hv+03PVw03gdB04t89/1O/w1cDnyilFU='
@@ -58,11 +60,38 @@ def response_message(event):
         alt_text='template',
         template=CarouselTemplate(columns=notes),
     )
+
+    '''
     if event.message.text == "PPAP":
         line_bot_api.reply_message(event.reply_token, messages=messages)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="isnot PPAP"))
+    '''
+    search_word = "LGgram"
+    try:
+        result_message = ""
+        result_list = mercariSearch(search_word)
+        for result in result_list:
+            result_message += result[0] + "\n" + result[1] + "\n"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = result_message))
+    except:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "検索出来ませんでした"))
 
+def mercariSearch(search_word):
+    result_list = []
+    ## https://www.mercari.com/jp/search/?keyword=LGgram
+    page = 'https://www.mercari.com/jp/search/?keyword={0}'.format(search_word)
+ 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    res = requests.get(page, headers=headers)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text)
+    elems_name = soup.select('.items-box-name')
+    elems_price = soup.select('.items-box-price')
+    for i in range(len(elems_name)):
+        result_list.extend([elems_name[i].text, elems_price[i].text])
+
+    return result_list
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))

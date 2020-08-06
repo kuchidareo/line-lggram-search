937b9d5e3dfcadcbba79e36524558e0099598867
Reo Kuchida <>
2020-08-06 10:41:39 +0900

    Remove unused code and move position of mercariSearch

--- main.py
+++ main.py
@@ -21,6 +21,22 @@ LINE_CHANNEL_SECRET = '708857c7a0cff5555d7bea327d126b2a'
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+def mercariSearch(search_word):
+    result_list = []
+    ## https://www.mercari.com/jp/search/?keyword=LGgram
+    page = 'https://www.mercari.com/jp/search/?keyword={0}'.format(search_word)
+ 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    res = requests.get(page, headers=headers)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text)
+    elems_name = soup.select('.items-box-name')
+    elems_price = soup.select('.items-box-price')
+    for i in range(len(elems_name)):
+        result_list.append([elems_name[i].text, elems_price[i].text])
+
+    return result_list
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -39,38 +55,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def response_message(event):
-    # notesのCarouselColumnの各値は、変更してもらって結構です。
-    '''notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
-                            title="【ReleaseNote】トークルームを実装しました。",
-                            text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
-                            actions=[{"type": "message","label": "サイトURL","text": "https://renttle.jp/notes/kota/7"}]),
-
-             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
-                            title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
-                            text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
-                            actions=[
-                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
-
-             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
-                            title="【ReleaseNote】タグ機能を追加しました。",
-                            text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
-                            actions=[
-                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])]
-
-    messages = TemplateSendMessage(
-        alt_text='template',
-        template=CarouselTemplate(columns=notes),
-    )
-    '''
-
-    '''
-    if event.message.text == "PPAP":
-        line_bot_api.reply_message(event.reply_token, messages=messages)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="isnot PPAP"))
-    '''
     search_word = "LGgram"
     try:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "検索を開始しました"))
         result_message = ""
         result_list = mercariSearch(search_word)
         for result in result_list:
@@ -78,22 +65,6 @@ def response_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = result_message))
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "検索出来ませんでした"))
-
-def mercariSearch(search_word):
-    result_list = []
-    ## https://www.mercari.com/jp/search/?keyword=LGgram
-    page = 'https://www.mercari.com/jp/search/?keyword={0}'.format(search_word)
- 
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    res = requests.get(page, headers=headers)
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text)
-    elems_name = soup.select('.items-box-name')
-    elems_price = soup.select('.items-box-price')
-    for i in range(len(elems_name)):
-        result_list.append([elems_name[i].text, elems_price[i].text])
-
-    return result_list
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))

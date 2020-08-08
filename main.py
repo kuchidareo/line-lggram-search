a1c636b4b742c6a79c7f52b759baa839d28f1e64
Reo Kuchida <>
2020-08-08 20:35:09 +0900

    Add raw data print

--- main.py
+++ main.py
@@ -108,6 +108,7 @@ def response_message(event):
     try:
         result_list = searchUsedMarket(search_word_list)
         result_list.sort(key=lambda x: x[1]) # priceでsort
+        line_bot_api.push_message(USER_ID, TextSendMessage(text=result_list))
         size = 10 # Max10個までなので
         for start in range(0, len(result_list), size):
             notes = []
@@ -115,7 +116,7 @@ def response_message(event):
             for result in ten_digit_result_list:
                 new_column = CarouselColumn(thumbnail_image_url = result[3],
                                             title = result[0],
-                                            text = result[1], # set number format
+                                            text = "{:,}".format(result[1]), # set number format
                                             actions = [URIAction(label='詳しく見る',uri=result[2])])
                 notes.append(new_column)
             messages = TemplateSendMessage(
@@ -123,7 +124,6 @@ def response_message(event):
                             template=CarouselTemplate(columns=notes),
                             )
             line_bot_api.push_message(USER_ID, messages=messages)
-            time.sleep(10)
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "検索出来ませんでした"))
 
